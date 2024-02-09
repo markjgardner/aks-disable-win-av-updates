6a029a3ded4b81294b614af5687a6ce6a31cabb6
Mark Gardner <mark.gardner@microsoft.com>
2024-02-09 14:56:31 -0500

    bugfix

--- nodeController/main.go
+++ nodeController/main.go
@@ -41,45 +41,36 @@ func main() {
 
 	// get all pods with the label app=disable-av-signature-updates on the target node
 	poll := true
-	var pods *corev1.PodList
 	for poll {
 		pods, _ := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{LabelSelector: "app=disable-av-signature-updates", FieldSelector: "spec.nodeName=" + nodeName})
-		if len(pods.Items) != 0 {
-			fmt.Fprintf(os.Stdout, "Found %d pods on host %s\n", len(pods.Items), nodeName)
+		if len(pods.Items) == 1 {
 			poll = false
-		} else {
-			fmt.Fprintf(os.Stdout, "Waiting for pods to be scheduled on host %s\n", nodeName)
-			time.Sleep(5 * time.Second)
-		}
-	}
-
-	if len(pods.Items) == 1 {
-		pod := &pods.Items[0]
-
-		waiting := true
-		// wait for the pod to complete
-		for waiting {
-			if pod.Status.Phase == "Succeeded" {
-				// remove the taint and add label
-				node, _ := clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
-				node.Spec.Taints = removeTaint(node.Spec.Taints, "canary")
-				node.Labels["winops/canary-status"] = "complete"
-				_, err := clientset.CoreV1().Nodes().Update(context.Background(), node, metav1.UpdateOptions{})
-				if err != nil {
-					_, _ = fmt.Fprintf(os.Stderr, "Error: failed to update node, retry in 10 seconds\n")
-					time.Sleep(10 * time.Second)
+			pod := &pods.Items[0]
+			waiting := true
+			// wait for the pod to complete
+			for waiting {
+				if pod.Status.Phase == "Succeeded" {
+					// remove the taint and add label
+					node, _ := clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
+					node.Spec.Taints = removeTaint(node.Spec.Taints, "canary")
+					node.Labels["winops/canary-status"] = "complete"
+					_, err := clientset.CoreV1().Nodes().Update(context.Background(), node, metav1.UpdateOptions{})
+					if err != nil {
+						_, _ = fmt.Fprintf(os.Stderr, "Error: failed to update node, retry in 10 seconds\n")
+						time.Sleep(10 * time.Second)
+					} else {
+						fmt.Fprintf(os.Stdout, "Pod %s is complete, removed taint and added label\n", pod.Name)
+						waiting = false
+					}
 				} else {
-					fmt.Fprintf(os.Stdout, "Pod %s is complete, removed taint and added label\n", pod.Name)
-					waiting = false
+					fmt.Fprintf(os.Stdout, "Pod %s is not complete, waiting 10 seconds\n", pod.Name)
+					time.Sleep(10 * time.Second)
+					pod, _ = clientset.CoreV1().Pods("default").Get(context.Background(), pod.Name, metav1.GetOptions{})
 				}
-			} else {
-				fmt.Fprintf(os.Stdout, "Pod %s is not complete, waiting 10 seconds\n", pod.Name)
-				time.Sleep(10 * time.Second)
-				pod, _ = clientset.CoreV1().Pods("default").Get(context.Background(), pod.Name, metav1.GetOptions{})
 			}
+		} else {
+			_, _ = fmt.Fprintf(os.Stderr, "Error: more than one canary pod found")
 		}
-	} else {
-		_, _ = fmt.Fprintf(os.Stderr, "Error: more than one canary pod found")
 	}
 
 	os.Exit(0)
